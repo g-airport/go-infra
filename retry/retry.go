dcbb41b1b81afcbc39e57d37ad809f3ce969d3df
Leo ll <dddddonly8@gmail.com>
2019-03-06 10:49:33 +0800

    add retry

---
+++ retry/retry.go
@@ -0,0 +1,33 @@
+package retry
+
+import (
+	"math/rand"
+	"time"
+
+	"github.com/go-infra/log"
+)
+
+func Retry(attempts int, sleep time.Duration, f func() error,
+	logFunc func(error) string) error {
+	err := f()
+	if err == nil {
+		return nil
+	}
+
+	if attempts--; attempts <= 0 {
+		return err
+	}
+
+	// Output warn logs
+	output := logFunc(err)
+	if output != "" {
+		log.Warnw("retry", output)
+	}
+
+	// Add some randomness to prevent creating a Thundering Herd
+	jitter := time.Duration(rand.Int63n(int64(sleep)))
+	sleep = sleep + jitter/2
+
+	time.Sleep(sleep)
+	return Retry(attempts, 2*sleep, f, logFunc)
+}
