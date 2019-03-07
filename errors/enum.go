16f5569e7acbed8d4ef4885887ba857b9a8e20d5
Leo ll <dddddonly8@gmail.com>
2019-03-07 20:53:22 +0800

    split micro err

--- errors/enum.go
+++ errors/enum.go
@@ -1,1 +1,6 @@
 package errors
+
+var (
+	ErrInvalidPing      = BadRequest(50001, "bad ping")
+	ErrDecodeArgsFailed = BadRequest(50002, "decode args failed")
+)
--- errors/merr.go
+++ errors/merr.go
@@ -1,1 +1,33 @@
 package errors
+
+import (
+	"encoding/json"
+
+	errM "github.com/micro/go-micro/errors"
+)
+
+// micro server
+
+func ParseRPCError(err error) *Error {
+	if err == nil {
+		return nil
+	}
+
+	errTxt := err.Error()
+
+	// split "micro error" layer
+	mErr := errM.Parse(errTxt)
+	errTxt = mErr.Detail
+
+	// try to parse to our service error
+	return TryParse(errTxt)
+}
+
+func TryParse(err string) *Error {
+	e := new(Error)
+	if err := json.Unmarshal([]byte(err), e); err != nil {
+		return nil
+	}
+
+	return e
+}
