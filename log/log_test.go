aef03b480769e613f3228d8b05cca3a51bee7911
Leo ll <dddddonly8@gmail.com>
2019-03-08 13:41:02 +0800

    add log test

---
+++ log/log_test.go
@@ -0,0 +1,26 @@
+package log
+
+import (
+	"fmt"
+	"path/filepath"
+	"testing"
+
+	"github.com/go-infra/env"
+)
+
+var Access *Logger
+
+func init() {
+
+	var err error
+	path, _ := filepath.Abs(env.LogDir + "/access.log")
+	fmt.Println(path)
+	Access, err = NewLogger(path, "debug")
+	if err != nil {
+		panic("new log failed")
+	}
+}
+
+func TestLog(t *testing.T) {
+	Access.Info("access")
+}
