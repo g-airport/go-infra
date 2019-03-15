3f2f291b23975ca3746263b27c150d923e804536
Leo ll <dddddonly8@gmail.com>
2019-03-15 20:50:32 +0800

    add orm of db builder

---
+++ builder/db.go
@@ -0,0 +1,66 @@
+package builder
+
+import (
+	"time"
+
+	_ "github.com/go-sql-driver/mysql" // default use mysql
+
+	"database/sql"
+	"github.com/go-xorm/xorm"
+	"github.com/jinzhu/gorm"
+)
+
+// support native db, xorm, gorm
+type DBBuilder struct {
+	Driver          string
+	Url             string
+	MaxOpenConns    int
+	MaxIdleConns    int
+	ConnMaxLifetime time.Duration
+}
+
+func (o DBBuilder) BuildDB() (*sql.DB, error) {
+	db, err := sql.Open(o.Driver, o.Url)
+	if err != nil {
+		return nil, err
+	}
+	db.SetMaxOpenConns(o.MaxOpenConns)
+	db.SetMaxIdleConns(o.MaxIdleConns)
+	db.SetConnMaxLifetime(o.ConnMaxLifetime)
+	err = db.Ping()
+	if err != nil {
+		return nil, err
+	}
+	return db, nil
+}
+
+func (o DBBuilder) BuildXORM() (*xorm.Engine, error) {
+	db, err := xorm.NewEngine(o.Driver, o.Url)
+	if err != nil {
+		return nil, err
+	}
+	db.SetMaxOpenConns(o.MaxOpenConns)
+	db.SetMaxIdleConns(o.MaxIdleConns)
+	db.SetConnMaxLifetime(o.ConnMaxLifetime)
+	err = db.Ping()
+	if err != nil {
+		return nil, err
+	}
+	return db, nil
+}
+
+// recommended
+func (o DBBuilder) BuildGORM() (*gorm.DB, error) {
+	db, err := gorm.Open(o.Url)
+	if err != nil {
+		return nil, err
+	}
+	db.DB().SetMaxOpenConns(o.MaxOpenConns)
+	db.DB().SetMaxIdleConns(o.MaxIdleConns)
+	db.DB().SetConnMaxLifetime(o.ConnMaxLifetime)
+	err = db.DB().Ping()
+	if err != nil {
+		return nil, err
+	}
+	return db, nil
+}
